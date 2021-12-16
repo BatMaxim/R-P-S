e2fd89567a1fde3dbe8f568eb83b646cd5fc5193
Maksim <gato-v@yandex.ru>
2021-12-16 15:51:05 +0300

    Added menu

---
+++ index.js
@@ -0,0 +1,50 @@
+require("colors");
+const InputCheck = require("./InputCheck");
+const RulesGenerator = require("./RulesGenerator");
+const TableGenerator = require("./TableGenerator");
+const GeneratorHMAC = require("./GeneratorHMAC");
+const readLineSync = require("readline-sync");
+const gameValues = process.argv.slice(2);
+
+if(!InputCheck.checkInputData(gameValues)){
+    InputCheck.printExample();
+    return;
+}
+
+const showMenu = () => {
+    gameValues.forEach((el, i)=>{
+        console.log(`${i+1}. ${el}`)
+    })
+    console.log(`0. Exit`)
+    console.log(`?. Help`)
+}
+
+const playGame = (index, bot) => {
+    console.log(`Your move: ${gameValues[index]}`);
+    console.log(`Computer move: ${gameValues[bot]}`);
+    const result = rulesGenerator.checkWinner(index, bot);
+    result === "Draw"? console.log(result): console.log(`You ${result}`);
+}
+
+const rulesGenerator = new RulesGenerator(gameValues.length);
+rulesGenerator.createRules();
+const tableGenerator = new TableGenerator(rulesGenerator.tableRules, gameValues);
+while(true){
+    const bot = Math.floor(Math.random() * gameValues.length);
+    const HMAC = new GeneratorHMAC();
+    HMAC.generateHMAC(gameValues[bot]);
+    console.log(`HMAC: ${HMAC.HMAC}`.yellow);
+    showMenu();
+    let index = readLineSync.keyIn('Enter your move: ',
+        {limit: `$<0-${gameValues.length}>$<?>`});
+    switch (index){
+        case "0":
+            return;
+        case "?":
+            tableGenerator.showTable();
+            break;
+        default:
+            playGame(index-1, bot)
+            console.log(` key: ${HMAC.key}\n`.yellow);
+    }
+}
